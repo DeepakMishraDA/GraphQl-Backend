882cb3a9df6bf471771e1677103fb2bcaf0128b1
deepak <deepakvinodmishra@gmail.com>
2023-01-03 00:40:55 +0100

    mockdata for api

---
+++ dist/database/mockData.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mockData = void 0;
+const mockData = () => {
+    return ["shree", "chreya", "panda", "dee", "babi", "curlina"];
+};
+exports.mockData = mockData;
